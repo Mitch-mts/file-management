4ddb5cfa26332a6f32597be56f599ee4994ed5e3
mitchell.severa <mitchell.severa@entelect.co.za>
2022-12-22 14:33:14 +0200

    removed unit tests

--- src/test/java/mts/mtech/filemanagement/FileManagementApplicationTests.java
+++ src/test/java/mts/mtech/filemanagement/FileManagementApplicationTests.java
@@ -1,13 +1,9 @@
 package mts.mtech.filemanagement;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FileManagementApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
